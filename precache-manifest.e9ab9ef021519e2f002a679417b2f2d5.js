9dc7ade135fde56c9a34fc0785bb6f25205e3cf4
john Merino <john.merino12345@yahoo.com>
2020-03-16 11:18:59 -0700

    Updates

---
+++ precache-manifest.e9ab9ef021519e2f002a679417b2f2d5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a467e8f1f363e23615c03f891cc72add",
+    "url": "/pokedex/index.html"
+  },
+  {
+    "revision": "77fc0dce40c4db245106",
+    "url": "/pokedex/static/css/main.d74a37c8.chunk.css"
+  },
+  {
+    "revision": "0a53bc34f634793edc51",
+    "url": "/pokedex/static/js/2.4b2c64d0.chunk.js"
+  },
+  {
+    "revision": "f231859d6585c4cd5f80c344783ed269",
+    "url": "/pokedex/static/js/2.4b2c64d0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "77fc0dce40c4db245106",
+    "url": "/pokedex/static/js/main.7b3779a8.chunk.js"
+  },
+  {
+    "revision": "c23ad90297b5c192407a",
+    "url": "/pokedex/static/js/runtime-main.52b07ced.js"
+  }
+]);
